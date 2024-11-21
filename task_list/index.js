dd3eb9917202b10632fbdd0fe2bd0e01f36c4d1e
GavrilS <gavr1lll@abv.bg>
2024-11-22 01:59:24 +0200

    Added functionality to select table rows and apply actions to the different buttons -> WIP

--- task_list/index.js
+++ task_list/index.js
@@ -1,5 +1,29 @@
-taskTable = document.querySelectorAll('.task-table-body');
-console.log('Table: \n', taskTable);
+taskTableRows = document.getElementById('tasks-table-body');
+console.log('Task table body:\n', taskTableRows);
+// console.log(typeof(taskTableRows));
 
-newTaskForm = document.querySelectorAll('#new-task-form');
-print('Form: \n', newTaskForm)
+for (const tr of taskTableRows.children) {
+    // console.log(tr.children[0]);
+    // console.log(tr.children[1]);
+    trButtons = tr.children[1];
+    editBtn = trButtons.children[0];
+    removeBtn = trButtons.children[1];
+    editBtn.addEventListener('click', (e) => {
+        editTask(tr);
+    });
+    removeBtn.addEventListener('click', (e) => {
+        removeTask(tr);
+    })
+    console.log(trButtons);
+    console.log('--------------------');
+}
+
+
+function editTask(e) {
+    console.log('Edit task event: ', e);
+}
+
+function removeTask(e) {
+    console.log('Remvoe task for row: ', e);
+    e.remove();
+}
