f6fb823718096e1e45ea108b1240477c5db09034
GavrilS <gavr1lll@abv.bg>
2024-11-22 00:55:31 +0200

    Adding code to handle adding/removing tasks to the table

---
+++ task_list/index.js
@@ -0,0 +1,5 @@
+taskTable = document.querySelectorAll('.task-table-body');
+console.log('Table: \n', taskTable);
+
+newTaskForm = document.querySelectorAll('#new-task-form');
+print('Form: \n', newTaskForm)
